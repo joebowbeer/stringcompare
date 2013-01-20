b67248e1d0673f1b476abd8a7ba31434502bfa64
Joe Bowbeer <joe.bowbeer@gmail.com>
2013-01-20 14:48:05 -0800

    initial creation

---
+++ src/stringcompare/StringCompare.java
@@ -0,0 +1,26 @@
+package stringcompare;
+
+import static java.util.stream.Streams.intRange;
+
+public class StringCompare {
+
+    public static void main(String[] args) {
+        System.out.println(compareMapReduce(args[0], args[1]));
+    }
+
+    public static int compareMapReduce(String s1, String s2) {
+        assert s1.length() == s2.length();
+        return intRange(0, s1.length())
+                .parallel()
+                .map(i -> Character.compare(s1.charAt(i), s2.charAt(i)))
+                .reduce(0, (l, r) -> (l != 0) ? l : r);
+    }
+
+    public static int compareBoxedReduce(String s1, String s2) {
+        assert s1.length() == s2.length();
+        return intRange(0, s1.length()).boxed()
+                .parallel()
+                .reduce(0, (l, i) -> (l != 0) ? l : Character.compare(s1.charAt(i), s2.charAt(i)),
+                           (l, r) -> (l != 0) ? l : r);
+    }
+}
