f3b602bfb60cea2935cbdea8ad023514593d92c5
Joe Bowbeer <joe.bowbeer@gmail.com>
2013-02-11 08:48:15 -0800

    .boxed().parallel()

--- src/stringcompare/StringCompare.java
+++ src/stringcompare/StringCompare.java
@@ -11,17 +11,14 @@ public class StringCompare {
 
     public static int compareMapReduce(String s1, String s2) {
         assert s1.length() == s2.length();
-        return intRange(0, s1.length())
-                .parallel()
+        return intRange(0, s1.length()).parallel()
                 .map(i -> compare(s1.charAt(i), s2.charAt(i)))
                 .reduce(0, (l, r) -> (l != 0) ? l : r);
     }
 
     public static int compareBoxedReduce(String s1, String s2) {
         assert s1.length() == s2.length();
-        return intRange(0, s1.length())
-                .boxed()
-                .parallel()
+        return intRange(0, s1.length()).parallel().boxed()
                 .reduce(0, (l, i) -> (l != 0) ? l : compare(s1.charAt(i), s2.charAt(i)),
                            (l, r) -> (l != 0) ? l : r);
     }
