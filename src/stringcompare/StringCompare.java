5821443e05e00901e313d4e77284615c7551f001
Joe Bowbeer <joe.bowbeer@gmail.com>
2013-05-05 23:55:46 -0700

    b88

--- src/stringcompare/StringCompare.java
+++ src/stringcompare/StringCompare.java
@@ -1,7 +1,7 @@
 package stringcompare;
 
 import static java.lang.Character.compare;
-import static java.util.stream.Streams.intRange;
+import java.util.stream.IntStream;
 
 public class StringCompare {
 
@@ -11,14 +11,14 @@ public class StringCompare {
 
     public static int compareMapReduce(String s1, String s2) {
         assert s1.length() == s2.length();
-        return intRange(0, s1.length()).parallel()
+        return IntStream.range(0, s1.length()).parallel()
                 .map(i -> compare(s1.charAt(i), s2.charAt(i)))
                 .reduce(0, (l, r) -> (l != 0) ? l : r);
     }
 
     public static int compareBoxedReduce(String s1, String s2) {
         assert s1.length() == s2.length();
-        return intRange(0, s1.length()).parallel().boxed()
+        return IntStream.range(0, s1.length()).parallel().boxed()
                 .reduce(0, (l, i) -> (l != 0) ? l : compare(s1.charAt(i), s2.charAt(i)),
                            (l, r) -> (l != 0) ? l : r);
     }
